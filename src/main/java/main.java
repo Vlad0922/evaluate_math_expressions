6875f6e86b9a1570632f7e2cda752a415a602e52
vlad0922 <vlad0922@yandex.ru>
2018-06-01 14:11:51 +0300

    [add] better comparison

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,10 +9,6 @@ import org.mariuszgromada.math.mxparser.*;
 
 import parsii.eval.*;
 
-import javax.script.Bindings;
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 
 public class main {
     public static double target_fun1(double a, double b) {
@@ -20,16 +16,17 @@ public class main {
     }
 
     public static void main(String[] args) {
+        final int RUN_NUMBER = 10000000;
 
         com.udojava.evalex.Expression expr_evalex = new com.udojava.evalex.Expression("SQRT(a^2 + b^2)");
 
-        for(int i = 0; i < 1000000; ++i) {
+        for(int i = 0; i < RUN_NUMBER; ++i) {
             BigDecimal res1 = expr_evalex.with("a", "2.4").and("b", "3.3").eval();
         }
 
         long startTime = System.nanoTime();
 
-        for(int i = 0; i < 1000000; ++i) {
+        for(int i = 0; i < RUN_NUMBER; ++i) {
             BigDecimal res1 = expr_evalex.with("a", "2.4").and("b", "3.3").eval();
         }
 
@@ -40,7 +37,7 @@ public class main {
         org.mariuszgromada.math.mxparser.Argument mx_b = new org.mariuszgromada.math.mxparser.Argument("b");
         org.mariuszgromada.math.mxparser.Expression exp_mx = new org.mariuszgromada.math.mxparser.Expression("sqrt(a^2 + b^2)", mx_a, mx_b);
 
-        for(int i = 0; i < 1000000; ++i) {
+        for(int i = 0; i < RUN_NUMBER; ++i) {
             mx_a.setArgumentValue(2.4);
             mx_b.setArgumentValue(3.3);
             double val = exp_mx.calculate();
@@ -48,7 +45,7 @@ public class main {
 
         startTime = System.nanoTime();
 
-        for(int i = 0; i < 1000000; ++i) {
+        for(int i = 0; i < RUN_NUMBER; ++i) {
             mx_a.setArgumentValue(2.4);
             mx_b.setArgumentValue(3.3);
             double val = exp_mx.calculate();
@@ -66,7 +63,7 @@ public class main {
         try {
             parsii.eval.Expression parsii_expr = parsii.eval.Parser.parse("sqrt(a^2 + b^2)", scope);
 
-            for(int i = 0; i < 1000000; ++i) {
+            for(int i = 0; i < RUN_NUMBER; ++i) {
                 a.setValue(2.4);
                 b.setValue(3.3);
                 double res3 = parsii_expr.evaluate();
@@ -74,7 +71,7 @@ public class main {
 
             startTime = System.nanoTime();
 
-            for(int i = 0; i < 1000000; ++i) {
+            for(int i = 0; i < RUN_NUMBER; ++i) {
                 a.setValue(2.4);
                 b.setValue(3.3);
                 double res3 = parsii_expr.evaluate();
@@ -88,13 +85,13 @@ public class main {
             System.out.println("wtf");
         }
 
-        for(int i = 0; i < 1000000; ++i) {
+        for(int i = 0; i < RUN_NUMBER; ++i) {
             double res2 = target_fun1(2.4, 3.3);
         }
 
         startTime = System.nanoTime();
 
-        for(int i = 0; i < 1000000; ++i) {
+        for(int i = 0; i < RUN_NUMBER; ++i) {
             double res2 = target_fun1(2.4, 3.3);
         }
 
@@ -113,13 +110,13 @@ public class main {
             Class aClass = CompilerUtils.CACHED_COMPILER.loadFromJava(className, source);
             BiMathFunction func = (BiMathFunction)aClass.newInstance();
 
-            for(int i = 0; i < 1000000; i++) {
+            for(int i = 0; i < RUN_NUMBER; i++) {
                 double result = func.compute(2.3, 3.4);
             }
 
             startTime = System.nanoTime();
 
-            for(int i = 0; i < 1000000; i++) {
+            for(int i = 0; i < RUN_NUMBER; i++) {
                 double result = func.compute(2.3, 3.4);
             }
 
@@ -131,18 +128,22 @@ public class main {
             System.out.println("wtf3");
         }
 
-        System.out.println(evalexTime*1e-9);
-        System.out.println(mxTime*1e-9);
-        System.out.println(parsiiTime*1e-9);
-        System.out.println(compileTime*1e-9);
-        System.out.println(funcTime*1e-9);
+        double evalexAverage = 1.0*evalexTime/RUN_NUMBER;
+        double mxAverage = 1.0*mxTime/RUN_NUMBER;
+        double parsiiAverage = 1.0*parsiiTime/RUN_NUMBER;
+        double compiledAverage = 1.0*compileTime/RUN_NUMBER;
+        double pureAverage = 1.0*funcTime/RUN_NUMBER;
+
+        System.out.format("Evalex result: %.5fns\n",    evalexAverage);
+        System.out.format("mxParser result: %.5fns\n",  mxAverage);
+        System.out.format("Parsii result: %.5fns\n",    parsiiAverage);
+        System.out.format("Compiled function result: %.5fns\n", compiledAverage);
+        System.out.format("Pure function result: %.5fns\n",     pureAverage);
 
 
-        System.out.println(1.0*evalexTime/funcTime);
-        System.out.println(1.0*mxTime/funcTime);
-        System.out.println(1.0*parsiiTime/funcTime);
-        System.out.println(1.0*compileTime/funcTime);
+        System.out.format("Evalex to pure function ratio: %.3f\n",      evalexAverage/pureAverage);
+        System.out.format("mxParser to pure function ratio: %.3f\n",    mxAverage/pureAverage);
+        System.out.format("Parsii to pure function ratio: %.3f\n",      parsiiAverage/pureAverage);
+        System.out.format("compiled function to pure ratio: %.3f\n",    compiledAverage/pureAverage);
     }
-
-
 }
